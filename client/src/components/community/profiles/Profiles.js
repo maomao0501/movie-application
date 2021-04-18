70ebe50769feb759babcbf5ef16fb84122f29409
Huilian-J <jiang.hui@northeastern.edu>
2021-04-18 00:42:55 -0400

    Revert "community profile rearrangement"
    This reverts commit eeab40355ca9438e214f296db3b43359b707331c.

--- client/src/components/community/profiles/Profiles.js
+++ client/src/components/community/profiles/Profiles.js
@@ -22,27 +22,8 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
                 See how people think about MovieDB here
           </p>
             </div>
-            {/*<div className="col-sm-4">*/}
-            {/*  <div className='profile'>*/}
-            {/*    <img src={avatar} alt='' className='profile-img round-img' />*/}
-            {/*    <div>*/}
-            {/*      <Link to={`/profile/${_id}`}>{name}</Link>*/}
-            {/*      <p className='my-1'>My bio: {bio}</p>*/}
-            {/*      <p>Favorite movie genre:</p>*/}
-            {/*      <ul>*/}
-            {/*        {movieTag.slice(0, 4).map((tag, index) => (*/}
-            {/*            <li key={index} className='text-primary'>*/}
-            {/*              {tag}*/}
-            {/*            </li>*/}
-            {/*        ))}*/}
-            
-            {/*      </ul>*/}
-            {/*    </div>*/}
-
-            {/*  </div>*/}
-            {/*</div>*/}
             <div className='profiles'>
-              <ul className="list-group">
+              <div className="row">
                 {profiles.length > 0 ? (
                   profiles.map(profile => (
                     <ProfileItem key={profile._id} profile={profile} />
@@ -50,7 +31,7 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
                 ) : (
                     <h4>No profiles found...</h4>
                   )}
-              </ul>
+              </div>
             </div>
           </Fragment>
         )}
